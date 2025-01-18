c8d4d810f4fd0cd8b9e07958fe70dbdabf470c19
Cain <cainmartinez2@hotmail.com>
2025-01-18 03:26:16 +0100

    Symfony: add dependencies

--- Event/backend-symfony/config/bundles.php
+++ Event/backend-symfony/config/bundles.php
@@ -7,5 +7,5 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    IS\Bumbu\BumbuBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
