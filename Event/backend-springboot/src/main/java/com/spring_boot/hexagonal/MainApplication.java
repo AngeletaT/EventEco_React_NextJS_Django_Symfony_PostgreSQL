b1b17802185839b84eda3cff08b9ace15904af71
Cain <cainmartinez2@hotmail.com>
2025-02-05 00:56:07 +0100

    SpringBoot: Replace MainApplication with Hexagonal architecture implementation

--- Event/backend-springboot/src/main/java/com/spring_boot/hexagonal/MainApplication.java
+++ Event/backend-springboot/src/main/java/com/spring_boot/hexagonal/MainApplication.java
@@ -1,10 +1,10 @@
-package com.spring_boot.event;
+package com.spring_boot.hexagonal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * The main class for the Eventeco Spring Boot application.
+ * The main class for the Hexagonal Spring Boot application.
  */
 @SpringBootApplication
 public class MainApplication {
