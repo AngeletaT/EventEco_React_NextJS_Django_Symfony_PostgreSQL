e2d8463959050f9a32b6f2af2433fdf5421fb4b4
Angela Torro <a.torroballester@gmail.com>
2025-02-05 20:44:44 +0100

    NextJS: Refactor useCategories hook to simplify query usage; update global styles with gradients and adjust Home module styles for improved layout and aesthetics

--- Event/full-stack-nextjs/src/hooks/eventeco/useCategories.ts
+++ Event/full-stack-nextjs/src/hooks/eventeco/useCategories.ts
@@ -4,10 +4,9 @@ import { useQuery } from "@tanstack/react-query";
 import { getCategories } from "@/services/eventeco/queries/getCategories";
 import { Category } from "@/types/Category";
 
-export const useCategories = <T extends Category[] = Category[]>() => {
-    return useQuery<T>({
+export const useCategories = <T extends Category[] = Category[]>() =>
+    useQuery({
         queryKey: ["categories"],
         queryFn: getCategories as () => Promise<T>,
         staleTime: 1000 * 60 * 5,
     });
-};
