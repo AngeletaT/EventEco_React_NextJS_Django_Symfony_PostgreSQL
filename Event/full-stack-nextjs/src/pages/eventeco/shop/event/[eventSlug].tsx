b10c3bce0e53b4df5cc00b3fc98839901feccf6c
Angela Torro <a.torroballester@gmail.com>
2025-02-11 17:21:13 +0100

    NextJS: Add EventecoDetailsClient component and related styles; implement event details page with static generation and metadata support.

--- Event/full-stack-nextjs/src/pages/eventeco/shop/event/[eventSlug].tsx
+++ Event/full-stack-nextjs/src/pages/eventeco/shop/event/[eventSlug].tsx
@@ -1,16 +1,56 @@
+import { GetStaticPaths, GetStaticProps } from "next";
 import { Metadata } from "next";
+import EventecoDetailsClient from "@/components/eventeco/event/EventecoDetailsClient";
+import { getEventBySlug, getEvents } from "@/services/eventeco/queries/getEvents";
+import { Event } from "@/types/Event";
 
 export const metadata: Metadata = {
-    title: "Details Event",
+    title: "EventEco - Detalles",
+    description: "Detalles del evento.",
 };
 
-export default async function Page(props: { params: Promise<{ eventslug: string }> }) {
-    const params = await props.params;
-    const eventslug = params.eventslug;
+export const getStaticPaths: GetStaticPaths = async () => {
+    const events = await getEvents();
+    const paths = events.map((event) => ({
+        params: { eventslug: event.eventslug },
+    }));
 
-    return (
-        <section>
-            <h1>event Details {eventslug}</h1>
-        </section>
-    );
+    return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    const { eventslug } = context.params as { eventslug: string };
+    const event = await getEventBySlug(eventslug);
+
+    return {
+        props: {
+            event,
+        },
+    };
+};
+
+export async function generateMetadata({ params }: EventDetailsPageProps): Promise<Metadata> {
+    const event = await getEventBySlug(params.eventSlug);
+    return {
+        title: event.title,
+        description: event.description,
+        openGraph: {
+            title: event.title,
+            description: event.description,
+            images: [
+                {
+                    url: event.image,
+                    width: 800,
+                    height: 600,
+                    alt: event.title,
+                },
+            ],
+        },
+    };
 }
+
+const EventecoDetaisPage = ({ event }: { event: Event }) => {
+    return <EventecoDetailsClient event={event} />;
+};
+
+export default EventecoDetaisPage;
